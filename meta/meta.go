fa7103cea4f35a0c3ebcea9a49f7914f87d9b652
Adam P <adam.plazewski@gmail.com>
2018-07-05 23:40:45 -0600

    more meta changes

--- meta/meta.go
+++ meta/meta.go
@@ -3,9 +3,12 @@ package meta
 import (
 	"os"
 	"time"
+	"mime"
 	"io/ioutil"
 	"encoding/xml"
+	"path/filepath"
 	"blunders"
+	"fmt"
 )
 
 type Meta struct {
@@ -29,10 +32,21 @@ func NewMeta(meta_location string) (meta Meta) {
 	meta.Blunders.AddCode(2, "Unmarshal")
 	
 	
+	meta_type := mime.TypeByExtension(filepath.Ext(meta_location))
+	if meta_type != "application/xml" {
+		meta.Blunders.NewFatal(1, "Meta file not in xml format. Trying to use: "+meta_location)
+	}
+
 	file, file_error := os.Open(meta_location)
 	if file_error != nil {
 		meta.Blunders.NewFatal(1, "Unable to open Meta File: "+file_error.Error())
 	}
+
+	file_stats, file_stat_error := file.Stat()
+	if file_stat_error != nil {
+		meta.Blunders.NewFatal(1, "Unable to open Meta File: "+file_stat_error.Error())
+	}
+	meta.DataAge = file_stats.ModTime()
 
 	byte_val, read_error := ioutil.ReadAll(file)
 	if read_error != nil {
@@ -43,5 +57,27 @@ func NewMeta(meta_location string) (meta Meta) {
 	if unmarshal_error != nil {
 		meta.Blunders.NewFatal(2, "Unable to Unmarshal meta data: "+unmarshal_error.Error())
 	}
+
+	meta.PointPositions = make(map[string]int)
+	for _, dp := range meta.DataPoints {
+		meta.PointPositions[dp.Name] = dp.Position
+	}
+
 	return
+}
+
+func (meta Meta) DisplayMeta() {
+	fmt.Println("Location:", meta.DataLocation)
+	fmt.Println("IsFolder:", meta.LocationIsFolder)
+	fmt.Println("MimeType:", meta.DataType)
+	fmt.Println("DataAge:", meta.DataAge)
+
+	for _, dp := range meta.DataPoints {
+		fmt.Println("	", dp)
+	}
+
+	for point, position := range meta.PointPositions {
+		fmt.Println("	", point, " => ", position)
+	}
+
 }
