e7175643adcb53ececc42e118e79a2622b3818ca
Adam P <adam.plazewski@gmail.com>
2019-03-24 00:40:17 -0600

    working read and write csv data logic

--- csv/csv.go
+++ csv/csv.go
@@ -1,11 +1,11 @@
+// data/csv package is a simple wrapper for the builtin csv package.
+// simplifies a lot of the typical and basic logic of reading and writing csv data.
 package csv
 
 import (
 	"os"
 	"bufio"
 	"encoding/csv"
-//	"path/filepath"
-//	"time"
 )
 
 type Csv struct {
@@ -16,6 +16,7 @@ type Csv struct {
 
 	fileReference *os.File
 	reader *csv.Reader
+	writer *csv.Writer
 }
 
 func New(data_location string) (new_csv Csv) {
@@ -35,8 +36,7 @@ func LoadNewData(data_location string) (new_csv Csv) {
 	return
 }
 
-// LoadData opens the file and reads the first record.
-// The first record is read into Cav.ActiveRecord
+// LoadData opens the file and reads the first record into Csv.ActiveRecord
 func (c *Csv) LoadData() () {
 	// Open csv file
 	file_reference, file_open_error := os.Open(c.Path)
@@ -53,7 +53,7 @@ func (c *Csv) LoadData() () {
 	c.LoadNextRecord()
 
 	return
-} 
+}
 
 // LoadNextRecord populates the ActiveRecord value.
 // Sets HasMoreRecords value
@@ -75,3 +75,42 @@ func (c *Csv) LoadNextRecord() {
 /////////////////////////////////////////////////////////
 //// Write Functions
 /////////////////////////////////////////////////////////
+
+// WriteNewData is a quick way of creating a new Csv instance and Writer in the same call.
+func WriteNewData(data_location string) (new_csv Csv) {
+	new_csv = New(data_location)
+	new_csv.WriteData()
+	return
+}
+
+// WriteData opens the file and creates the csv reference.
+// If the file does not exist it is created.
+func (c *Csv) WriteData() () {
+	// Open csv file
+	file_reference, file_open_error := os.Create(c.Path)
+	if (file_open_error != nil) {
+		c.Errors = append(c.Errors, file_open_error)
+		return
+	}
+	c.fileReference = file_reference
+
+	// Read file reference into csv reader
+	c.writer = csv.NewWriter(c.fileReference)
+	
+	return
+}
+
+// Write Record writes the rovided record to the csv file reference.
+// The data will not actually be written to the file until WriteRecordsToFile is called.
+func (c *Csv) WriteRecord(record []string) {
+	write_error := c.writer.Write(record)
+	if write_error != nil {
+		c.Errors = append(c.Errors, write_error)
+	}
+}
+
+// WriteRecordsToFile is a simple wrapper for Flush.
+// I'm sure a better way to do this is possible.
+func (c *Csv) WriteRecordsToFile() {
+	c.writer.Flush()
+}
